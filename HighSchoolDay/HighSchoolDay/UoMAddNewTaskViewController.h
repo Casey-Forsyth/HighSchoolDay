e2b38c7a86d7a05adaf57c678636edd16447e0e0
Casey Forsyth <caseyvill@gmail.com>
2014-05-22 20:16:37 -0500

    Added Class Files

---
+++ HighSchoolDay/HighSchoolDay/UoMAddNewTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  UoMAddNewTaskViewController.h
+//  HighSchoolDay
+//
+//  Created by Casey Forsyth on 2014-05-22.
+//  Copyright (c) 2014 UofM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UoMAddNewTaskViewController : UIViewController
+
+@end
---
+++ HighSchoolDay/HighSchoolDay/UoMToDoList.h
@@ -0,0 +1,13 @@
+//
+//  UoMToDoList.h
+//  HighSchoolDay
+//
+//  Created by Casey Forsyth on 2014-05-22.
+//  Copyright (c) 2014 UofM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UoMToDoList : UITableViewController
+
+@end
